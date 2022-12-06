56cddd7c57006774fa6149d9433dc7c15ab9dab8
Bastiaan Rudolf Witte <baswitte1@live.nl>
2022-12-06 10:56:23 +0100

    Day 6 - TIL next()

--- day6/solution.py
+++ day6/solution.py
@@ -1,5 +1,5 @@
 with open('data.txt') as f:
     lines = f.readlines()
 
-print(f"Start of packet marker: {[i for i, char in enumerate(lines[0]) if len(set([j for j in lines[0][i-4:i]])) == 4][0]}")
-print(f"Start of message marker: {[i for i, char in enumerate(lines[0]) if len(set([j for j in lines[0][i-14:i]])) == 14][0]}")
+print(f"Start of packet marker: {next(i for i, char in enumerate(lines[0]) if len(set([j for j in lines[0][i-4:i]])) == 4)}")
+print(f"Start of message marker: {next(i for i, char in enumerate(lines[0]) if len(set([j for j in lines[0][i-14:i]])) == 14)}")
