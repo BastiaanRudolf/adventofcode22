f09278274087e3553f2b1cbccf1f7c99b5975e71
Bastiaan Rudolf Witte <baswitte1@live.nl>
2022-12-06 10:57:48 +0100

    Day 6 - even shorter

--- day6/solution.py
+++ day6/solution.py
@@ -1,5 +1,4 @@
 with open('data.txt') as f:
     lines = f.readlines()
 
-print(f"Start of packet marker: {next(i for i, char in enumerate(lines[0]) if len(set([j for j in lines[0][i-4:i]])) == 4)}")
-print(f"Start of message marker: {next(i for i, char in enumerate(lines[0]) if len(set([j for j in lines[0][i-14:i]])) == 14)}")
+print(f"{[next(i for i, char in enumerate(lines[0]) if len(set([j for j in lines[0][i-n:i]])) == n) for n in (4,14)]}")
